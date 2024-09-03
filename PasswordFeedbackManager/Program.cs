937563907743b8541c90c5f38400edc05035d7ed
KBakunina <114146888+KBakunina@users.noreply.github.com>
2024-09-03 21:58:36 +0500

    Initial commit

---
+++ PasswordFeedbackManager/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.OpenApi.Models;
+using PasswordFeedbackManager.Services;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IPasswordFeedbackService, PasswordFeedbackService>();
+builder.Services.AddMemoryCache();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PasswordFeedbackAPI", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
